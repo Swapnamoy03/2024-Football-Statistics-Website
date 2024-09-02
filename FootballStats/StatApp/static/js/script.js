d458ec7a1b075b72f0b3f052ead4e692849d56ab
Swapnamoy Raha <66420789+Swapnamoy03@users.noreply.github.com>
2024-09-02 21:37:25 +0530

    Add files via upload

---
+++ FootballStats/StatApp/static/js/script.js
@@ -0,0 +1,11 @@
+
+//For Background Audio
+/*window.addEventListener('DOMContentLoaded',()=>{
+    
+    setTimeout(() => {
+        myAudio = document.getElementById('x');
+        console.log(myAudio);
+        myAudio.volume = 0.9;
+        myAudio.play();
+    }, 1000);
+})*/
